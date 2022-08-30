76e4cdc0c387c27e71b8951f42e60188380fb337
W3LabKr <git@w3lab.kr>
2022-08-31 06:35:36 +0900

    Update croco.config.js

--- croco.config.js
+++ croco.config.js
@@ -3,15 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src/'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@css': path.resolve(__dirname, 'src/assets/css'),
-      '@images': path.resolve(__dirname, 'src/assets/images'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@atoms': path.resolve(__dirname, 'src/store/atoms'),
-      '@selectors': path.resolve(__dirname, 'src/store/selectors'),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
 };
